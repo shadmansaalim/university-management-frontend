8940888a5ff5be33d349dadce98f78145d433bc5
shadmansaalim <shadmansaalim999@gmail.com>
2023-10-30 16:51:43 +1100

    Ant Design styling configured

--- src/lib/Providers.tsx
+++ src/lib/Providers.tsx
@@ -3,9 +3,14 @@
 // Imports
 import { store } from "@/redux/store";
 import { Provider } from "react-redux";
+import StyledComponentsRegistry from "./AntdRegistry";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+    </Provider>
+  );
 };
 
 export default Providers;

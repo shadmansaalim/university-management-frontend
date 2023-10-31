a75062c5cd2a68e6bdea358f49392b78c040ab2d
shadmansaalim <shadmansaalim999@gmail.com>
2023-10-31 16:26:30 +1100

    Reusable breadcrumb component created for use in UI

--- src/components/ui/Contents.tsx
+++ src/components/ui/Contents.tsx
@@ -2,10 +2,29 @@
 
 // Imports
 import { Layout } from "antd";
+import UMBreadCrumb from "./UMBreadCrumb";
 const { Content } = Layout;
 
 const Contents = ({ children }: { children: React.ReactNode }) => {
-  return <Content style={{ color: "black" }}>{children}</Content>;
+  const base = "admin";
+
+  return (
+    <Content style={{ minHeight: "100vh", color: "black" }}>
+      <UMBreadCrumb
+        items={[
+          {
+            label: `${base}`,
+            link: `/${base}`,
+          },
+          {
+            label: "student",
+            link: `/${base}/student`,
+          },
+        ]}
+      />
+      {children}
+    </Content>
+  );
 };
 
 export default Contents;

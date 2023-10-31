4f73ca0c03f64eadf2a982bcadd75bc37ee37c06
shadmansaalim <shadmansaalim999@gmail.com>
2023-10-31 16:37:06 +1100

    Loading spinner implemented in loading.tsx for better UI/UX

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,9 @@
 const Loading = () => {
   return (
-    <div>
-      <h1>Loading....</h1>
+    <div className="spinner">
+      <div className="bounce1"></div>
+      <div className="bounce2"></div>
+      <div className="bounce3"></div>
     </div>
   );
 };

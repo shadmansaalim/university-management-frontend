9fd43d8215f03b0e7039afe268b78e21250bc1f8
shadmansaalim <shadmansaalim999@gmail.com>
2023-10-31 19:20:55 +1100

    Header implemented in user dashboard and logout feature implemented

--- src/components/ui/Contents.tsx
+++ src/components/ui/Contents.tsx
@@ -3,6 +3,7 @@
 // Imports
 import { Layout } from "antd";
 import UMBreadCrumb from "./UMBreadCrumb";
+import Header from "./Header";
 const { Content } = Layout;
 
 const Contents = ({ children }: { children: React.ReactNode }) => {
@@ -10,6 +11,7 @@ const Contents = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <Content style={{ minHeight: "100vh", color: "black" }}>
+      <Header />
       <UMBreadCrumb
         items={[
           {

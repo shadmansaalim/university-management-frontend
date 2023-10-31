0772be38bba4e0c46e22f64c2a99c24e93ae0aad
shadmansaalim <shadmansaalim999@gmail.com>
2023-10-31 16:17:26 +1100

    Login page created

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,67 @@
+"use client";
+
+// Imports
+import { Button, Col, Row } from "antd";
+import loginImage from "../../assets/login-image.png";
+import Image from "next/image";
+import Form from "@/components/Forms/Form";
+import FormInput from "@/components/Forms/FormInput";
+import { SubmitHandler } from "react-hook-form";
+
+type FormValues = {
+  id: string;
+  password: string;
+};
+
 const LoginPage = () => {
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    try {
+      console.log(data);
+    } catch (err) {}
+  };
   return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
+    <Row
+      justify="center"
+      align="middle"
+      style={{
+        minHeight: "100vh",
+      }}
+    >
+      <Col sm={12} md={16} lg={12} xl={10}>
+        <Image src={loginImage} width={500} alt="login image" />
+      </Col>
+      <Col sm={12} md={8} lg={8} xl={5}>
+        <h1
+          style={{
+            marginBottom: "16px",
+          }}
+        >
+          Login to access your portal
+        </h1>
+        <div>
+          <Form submitHandler={onSubmit}>
+            <div>
+              <FormInput name="id" type="text" size="large" label="User Id" />
+            </div>
+            <div
+              style={{
+                margin: "16px 0px",
+              }}
+            >
+              <FormInput
+                name="password"
+                type="password"
+                size="large"
+                label="User Password"
+              />
+            </div>
+            <Button type="primary" htmlType="submit">
+              Login
+            </Button>
+          </Form>
+        </div>
+      </Col>
+    </Row>
   );
 };
 

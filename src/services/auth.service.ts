7078078db023300fc4af92115d631fc43881bb94
shadmansaalim <shadmansaalim999@gmail.com>
2023-10-31 18:41:25 +1100

    Retrieving user logged in info from localstorage and removing

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,6 +1,27 @@
 // Imports
+import { authKey } from "@/constants/storageKey";
+import { decodedToken } from "@/utils/jwt";
 import { LocalStorageUtils } from "@/utils/local-storage";
 
 export const storeUserInfo = ({ accessToken }: { accessToken: string }) => {
-  LocalStorageUtils.set("accessToken", accessToken);
+  return LocalStorageUtils.set(authKey, accessToken);
+};
+
+export const getUserInfo = () => {
+  const authToken = LocalStorageUtils.get(authKey);
+  if (authToken) {
+    const decodedData = decodedToken(authToken);
+    return decodedData;
+  } else {
+    return "";
+  }
+};
+
+export const isLoggedIn = () => {
+  const authToken = LocalStorageUtils.get(authKey);
+  return !!authToken;
+};
+
+export const removeUserInfo = (key: string) => {
+  return LocalStorageUtils.del(key);
 };
--- src/utils/local-storage.ts
+++ src/utils/local-storage.ts
@@ -3,9 +3,24 @@ const set = (key: string, value: string) => {
   if (!key || typeof window === "undefined") {
     return "";
   }
-  localStorage.setItem(key, value);
+  return localStorage.setItem(key, value);
+};
+
+// Function to get local storage value from key
+const get = (key: string) => {
+  if (!key || typeof window === "undefined") {
+    return "";
+  }
+  return localStorage.getItem(key);
+};
+
+// Function to delete local storage value from key
+const del = (key: string) => {
+  return localStorage.removeItem(key);
 };
 
 export const LocalStorageUtils = {
   set,
+  get,
+  del,
 };

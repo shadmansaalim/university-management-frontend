51cf918c8223d3e30c2220a11874a8be99d344d0
shadmansaalim <shadmansaalim999@gmail.com>
2023-11-02 00:10:21 +1100

    Custom hook implemented to optimnize API call for departments search

--- src/app/(withlayout)/super_admin/department/page.tsx
+++ src/app/(withlayout)/super_admin/department/page.tsx
@@ -8,32 +8,51 @@ import { Button, Input } from "antd";
 import Link from "next/link";
 import ActionBar from "@/components/ui/ActionBar";
 import UMTable from "@/components/ui/UMTable";
-import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import {
+  EditOutlined,
+  DeleteOutlined,
+  ReloadOutlined,
+} from "@ant-design/icons";
 import { useDepartmentsQuery } from "@/redux/api/departmentApi";
 import { useState } from "react";
+import { useDebounced } from "@/redux/hooks";
 
 const ManageDepartmentPage = () => {
+  // User Role
   const { role } = getUserInfo() as IUserDecodedTokenData;
 
-  const query: Record<string, any> = {};
-
+  // States
   const [page, setPage] = useState<number>(1);
   const [size, setSize] = useState<number>(10);
   const [sortBy, setSortBy] = useState<string>("");
   const [sortOrder, setSortOrder] = useState<string>("");
   const [searchTerm, setSearchTerm] = useState<string>("");
 
+  // API call departments query
+  const query: Record<string, any> = {};
+
   query["limit"] = size;
   query["page"] = page;
   query["sortBy"] = sortBy;
   query["sortOrder"] = sortOrder;
-  query["searchTerm"] = searchTerm;
 
+  // Optimizing API call for user search
+  const debouncedTerm = useDebounced({
+    searchQuery: searchTerm,
+    delay: 600,
+  });
+
+  if (!!debouncedTerm) {
+    query["searchTerm"] = debouncedTerm;
+  }
+
+  // Getting all departments
   const { data, isLoading } = useDepartmentsQuery({ ...query });
 
   const departments = data?.departments;
   const meta = data?.meta as IMeta;
 
+  // Table columns
   const columns = [
     {
       title: "Title",
@@ -54,7 +73,7 @@ const ManageDepartmentPage = () => {
             <Link href={`/super_admin/department/edit/${data?.id}`}>
               <Button
                 style={{
-                  margin: "0px  5px 0px 0px",
+                  marginRight: "5px",
                 }}
                 onClick={() => console.log(data)}
                 type="primary"
@@ -72,16 +91,25 @@ const ManageDepartmentPage = () => {
     },
   ];
 
+  // Function that triggers when pagination related anything changes
   const onPaginationChange = (page: number, pageSize: number) => {
     setPage(page);
     setSize(pageSize);
   };
 
+  // Function that triggers when something on table changes
   const onTableChange = (pagination: any, filter: any, sorter: any) => {
     const { field, order } = sorter;
 
     setSortBy(field as string);
     setSortOrder(order === "ascend" ? "asc" : "desc");
+  };
+
+  // Function to reset user search and filters
+  const resetFilters = () => {
+    setSortBy("");
+    setSortOrder("");
+    setSearchTerm("");
   };
 
   return (
@@ -104,9 +132,16 @@ const ManageDepartmentPage = () => {
           }}
           onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <Link href="/super_admin/department/create">
-          <Button type="primary">Create Department</Button>
-        </Link>
+        <div>
+          <Link href="/super_admin/department/create">
+            <Button type="primary">Create</Button>
+          </Link>
+          {(!!sortBy || !!sortOrder || !!searchTerm) && (
+            <Button onClick={resetFilters} style={{ margin: "0px 5px" }}>
+              <ReloadOutlined />
+            </Button>
+          )}
+        </div>
       </ActionBar>
 
       <UMTable
